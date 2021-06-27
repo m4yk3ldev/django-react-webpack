08dad053d30370a7463409e3a9cca34a2a5c056b
Maykel Arias Torres <hanibal920915@gmail.com>
2021-06-26 21:18:02 -0400

    Cap6 completado

--- frontend/src/components/common/PrivateRoute.js
+++ frontend/src/components/common/PrivateRoute.js
@@ -5,20 +5,21 @@ import {connect} from "react-redux";
 const PrivateRoute = ({component: Component, auth, ...rest}) => (
     <Route
         {...rest}
-        render={(props) =>
-            localStorage.getItem("token") ? (
-                <Component {...props}/>
-            ) : (
-                <Redirect to="/login"/>
-            )
+        render={(props) => {
+            if (auth.isLoading) {
+                return <h2>Loading ...</h2>
+            } else if (!auth.isAuthenticated) {
+                return <Redirect to="/login"/>
+            } else {
+                return <Component {...props}/>
+            }
+        }
         }
     />
 );
-
 const mapStateToProps = state => (
     {
         auth: state.auth
     }
 )
-
 export default connect(mapStateToProps)(PrivateRoute);
