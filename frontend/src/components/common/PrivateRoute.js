da9ae24a6d41ce374126d90086bc0ad0b468d4ce
Maykel Arias Torres <hanibal920915@gmail.com>
2021-06-26 21:08:26 -0400

    Cap06 Error al cargar el dash ya logeado

--- frontend/src/components/common/PrivateRoute.js
+++ frontend/src/components/common/PrivateRoute.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Redirect, Route} from 'react-router-dom';
 import {connect} from "react-redux";
 
-const PrivateRoute = (component, auth, ...rest) => (
-
-    <Route {...rest} render={props => {
-
-        if (auth.isLoading) {
-            return <h2>Loading...</h2>;
-        } else if (!localStorage.getItem('token')) {
-            return <Redirect to="/login"/>;
-        } else {
-            return React.createElement(component, props);
+const PrivateRoute = ({component: Component, auth, ...rest}) => (
+    <Route
+        {...rest}
+        render={(props) =>
+            localStorage.getItem("token") ? (
+                <Component {...props}/>
+            ) : (
+                <Redirect to="/login"/>
+            )
         }
-    }}
     />
 );
 
